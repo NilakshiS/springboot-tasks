a22edca039186d0a0c6673d719e9da0566555ec3
Nilakshi <nilakshi.ma1509@ict.gnu.ac.in>
2019-07-26 12:12:31 +0530

    eighth-task

--- src/main/java/com/stackroute/Muzix/MuzixApplication.java
+++ src/main/java/com/stackroute/Muzix/MuzixApplication.java
@@ -4,20 +4,46 @@ import com.stackroute.Muzix.domain.Track;
 import com.stackroute.Muzix.exceptions.TrackAlreadyExistsException;
 import com.stackroute.Muzix.service.TrackService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+//set the property source so that compiler knows where to fetch values from
+@PropertySource("classpath:application.properties")
 public class MuzixApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
+	//TrackService dependency to save tracks in database
 	private TrackService trackService;
 
+	//autowire the setter to inject trackService dependency
 	@Autowired
 	public void setTrackService(TrackService trackService) {
 		this.trackService = trackService;
+	}
+
+	//setting values using @Value
+    @Value("${id1}")
+    private int id1;
+
+    @Value("${name1}")
+    private String name1;
+
+    @Value("${comment1}")
+    private String comment1;
+
+    //Environment can be used to get values from .properties file
+	private Environment environment;
+
+	//autowire the setter to inject environment dependency
+    @Autowired
+	public void setEnvironment(Environment environment) {
+		this.environment = environment;
 	}
 
 	@Override
@@ -32,9 +58,10 @@ public class MuzixApplication extends SpringBootServletInitializer implements Co
 	@Override
 	public void run(String... args){
 		try {
-			trackService.saveTrack(new Track(1, "Trivia:Love", "By RM"));
-			trackService.saveTrack(new Track(2, "Trivia:Seesaw", "By Suga"));
-			trackService.saveTrack(new Track(3, "Trivia:Just Dance", "By J-Hope"));
+			//insert one track using @value
+			trackService.saveTrack(new Track(id1, name1, comment1));
+			//insert the other track using environment.getProperty()
+			trackService.saveTrack(new Track(Integer.parseInt(environment.getProperty("id2")), environment.getProperty("name2"), environment.getProperty("comment2")));
 		} catch (TrackAlreadyExistsException e) {
 			e.printStackTrace();
 		}
